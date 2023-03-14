745bee2c561fafa662d5d9fcd78f9603d8e7468d
theopen4 <kielze86@gmail.com>
2023-03-14 18:23:08 +0100

    get the list of the articles and resolves problems of cors origin on backendpokemon

--- backendPokemon/app.js
+++ backendPokemon/app.js
@@ -1,6 +1,33 @@
 const express = require('express');
 const app = express();
-app.use((req, res) => {
-    res.json({message: 'votre requete a ete execute'})
-})
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
+
+  app.get('/api/pokemons/', (req, res) => {
+    const pokemons = [
+        {
+         id: 1,
+         name: "Bulbizarre",
+         hp: 25,
+         cp: 5,
+         picture: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/001.png",
+         types: ["Plante", "Poison"],
+         created: new Date()
+        },
+        {
+         id: 2,
+         name: "Salamèche",
+         hp: 28,
+         cp: 6,
+         picture: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/004.png",
+         types: ["Feu"],
+         created: new Date()
+        }
+    ]    
+    res.status(200).json(pokemons);
+  })
 module.exports = app;
