546a4d385a1db9756f764981b9ebe85648298a1c
theopen4 <kielze86@gmail.com>
2023-03-15 23:39:07 +0100

    connect daas atlas for backendPokemon

--- backendPokemon/app.js
+++ backendPokemon/app.js
@@ -1,5 +1,11 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+mongoose.mongoose.connect('mongodb+srv://Theopen:ezekiel1234@clusterpokemon.mi2uckb.mongodb.net/?retryWrites=true&w=majority',
+{ useNewUrlParser: true,
+  useUnifiedTopology: true })
+.then(() => console.log('connection a mongodb reussi !'))
+.catch(() => console.log('connection a mongodb echoue !'))
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
