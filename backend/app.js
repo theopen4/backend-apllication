50094b2cb860d7d19aa64d8b0474197101507e90
theopen4 <kielze86@gmail.com>
2023-03-16 00:37:27 +0100

    init mongodb model and modify post request

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
-
+const Thing = require('./models/thing')
 const app = express();
 mongoose.connect('mongodb+srv://Theopen4:ezekiel1234@clustertest.flnuido.mongodb.net/?retryWrites=true&w=majority',
   { useNewUrlParser: true,
@@ -15,11 +15,15 @@ app.use((req, res, next) => {
 });
 app.use(express.json());
 app.post('/api/stuff', (req, res, next) => {
-  console.log(req.body);
-  res.status(201).json({
-    message: 'Objet créé !'
+  delete req.body._id;
+  const thing = new Thing({
+    ...req.body
   });
-});
+  thing.save()
+    .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
+    .catch(error => res.status(400).json({ error }));
+})
+
 
 app.get('/api/stuff', (req, res, next) => {
   const stuff = [
