406e797de31f763795feae8d5794e27c8f5853b3
theopen4 <kielze86@gmail.com>
2023-03-18 19:58:50 +0100

    add the request put for modify a objet

--- backend/app.js
+++ backend/app.js
@@ -28,6 +28,11 @@ app.put('/api/stuff/:id', (req, res, next) => {
     .then(() => res.status(200).json({ message: 'Objet modifié !'}))
     .catch(error => res.status(400).json({ error }));
 });
+app.delete('/api/stuff/:id', (req, res, next) => {
+  Thing.deleteOne({ _id: req.params.id })
+    .then(() => res.status(200).json({ message: 'Objet supprimé !'}))
+    .catch(error => res.status(400).json({ error }));
+});
 app.get('/api/stuff/:id', (req, res, next) => {
   Thing.findOne({ _id: req.params.id })
     .then(thing => res.status(200).json(thing))
