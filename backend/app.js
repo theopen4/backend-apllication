25e6b58bac0a6bfd18912e36c80ceae7fc9af6cb
theopen4 <kielze86@gmail.com>
2023-03-20 12:43:42 +0100

    add model user and signed and controlers and routes in two backend

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const stuffRoutes = require('./routes/stuff');
 const app = express();
+const userRoutes = require('./routes/user');
 mongoose.connect('mongodb+srv://Theopen4:ezekiel1234@clustertest.flnuido.mongodb.net/?retryWrites=true&w=majority',
   { useNewUrlParser: true,
     useUnifiedTopology: true })
@@ -15,6 +16,7 @@ app.use((req, res, next) => {
 });
 app.use(express.json());
 app.use('/api/stuff', stuffRoutes);
+app.use('/api/auth', userRoutes);
 
 
 module.exports = app;
--- backendPokemon/app.js
+++ backendPokemon/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const Thing = require('./models/thing')
 const mongoose = require('mongoose');
+const userRoutes = require('./routes/user');
 const pokemonRoutes = require('./routes/pokemons')
 mongoose.mongoose.connect('mongodb+srv://Theopen:ezekiel1234@clusterpokemon.mi2uckb.mongodb.net/?retryWrites=true&w=majority',
 { useNewUrlParser: true,
@@ -17,6 +18,7 @@ app.use((req, res, next) => {
 
 app.use(express.json());
 app.use('/api/pokemons', pokemonRoutes);
+app.use('/api/auth', userRoutes);
 
 
 module.exports = app;
