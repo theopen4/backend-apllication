10662a11c69bbc74a658dc5406583f2cd8dc1144
theopen4 <kielze86@gmail.com>
2023-03-20 03:20:10 +0100

    strutury code backendpokemon

--- backendPokemon/app.js
+++ backendPokemon/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const Thing = require('./models/thing')
 const mongoose = require('mongoose');
+const pokemonRoutes = require('./routes/pokemons')
 mongoose.mongoose.connect('mongodb+srv://Theopen:ezekiel1234@clusterpokemon.mi2uckb.mongodb.net/?retryWrites=true&w=majority',
 { useNewUrlParser: true,
   useUnifiedTopology: true })
@@ -15,36 +16,7 @@ app.use((req, res, next) => {
   });
 
 app.use(express.json());
-app.post('/api/pokemons', (req, res) => {
-  delete req.body.id;
-  const thing = new Thing({
-       ...req.body
-  });
-  thing.save()
-    .then(() => {
-      res.status(201).json({thing, message: 'Pokemon enregistred'});
-    })
-    .catch(error => res.status(400).json({error}));
-    
-})
-app.put('/api/pokemons/:id', (req,  res) => {
-  Thing.updateOne({_id: req.params.id}, { ...req.body, _id: req.params.id})
-      .then(() => res.status(200).json({message: 'objet modifie'}))
-      .catch(error => res.status(400).json({error}))
-});
-app.delete('/api/pokemons/:id', (req, res, next) => {
-  Thing.deleteOne({ _id: req.params.id })
-    .then(() => res.status(200).json({ message: 'Objet supprimé !'}))
-    .catch(error => res.status(400).json({ error }));
-});
-app.get('/api/pokemons/:id', (req, res) => {
-  Thing.findOne({ _id: req.params.id})
-     .then(things => res.status(200).json(things))
-     .catch(error => res.status(404).json({error}));
-})
-app.use('/api/pokemons/', (req, res) => {
-  Thing.find()
-      .then(things => res.status(200).json(things))
-      .catch(error => res.status(400).json({error}));  
-  });
+app.use('/api/pokemons', pokemonRoutes);
+
+
 module.exports = app;
