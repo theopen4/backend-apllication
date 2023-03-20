a670f7ebc3cb09a64b0d5ff43d44f6f572393ddc
theopen4 <kielze86@gmail.com>
2023-03-20 13:08:20 +0100

    add model user and signed and controlers and routes in two backend

---
+++ backendPokemon/controllers/user.js
@@ -0,0 +1,21 @@
+const bcrypt = require('bcrypt');
+const User = require('../models/Users');
+
+exports.signup = (req, res, next) => {
+    bcrypt.hash(req.body.password, 10)
+       .then(hash => {
+          const user = new User({
+            email: req.body.email,
+            password: hash
+          });
+          user.save()
+             .then(() => res.status(201).json({message: 'Utulisateur cree '}))
+             .catch(err => res.status(400).json({err}));
+       })
+       .catch(err => res.status(500).json({err}));
+
+};
+
+exports.login = (req, res, next) => {
+
+};
