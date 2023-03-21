65b0af0bf1f01b0065278dc3719522d43a5a26d0
theopen4 <kielze86@gmail.com>
2023-03-21 21:42:55 +0100

    add authentification jwt

--- backendPokemon/controllers/user.js
+++ backendPokemon/controllers/user.js
@@ -30,7 +30,10 @@ exports.login = (req, res, next) => {
                   }
                   res.status(200).json({
                     userId: user._id,
-                    token: 'TOKEN'
+                    token: jwt.sign(
+                        {userId: user._id},
+                        'RANDOM_TOKEN_SECRET',
+                        { expiresIn: '24h' })
                   })  
                })
             .catch(error => res.status(500).json({error})) 
