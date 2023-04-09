a6e1e46da87f202b42010f1beb63d2995b0797ca
theopen4 <kielze86@gmail.com>
2023-04-09 17:52:47 +0100

    add class python

---
+++ poo_python/Tools.py
@@ -0,0 +1,10 @@
+class Rectangle:
+    def __init__(self, tool):
+        self.tool = tool
+
+    def add_tool(self, tool):
+        pass    
+
+    def remove_tool(self, tool):
+        pass
+        
